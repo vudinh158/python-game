25696a08ea676e9778b3801974044de8b601bc4d
phuocthien2304 <162446480+phuocthien2304@users.noreply.github.com>
2024-10-23 21:43:08 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,7 @@
+import GameBlockPuzzle
+import pygame
+win = pygame.display.set_mode(
+    (GameBlockPuzzle.s_width, GameBlockPuzzle.s_height))
+pygame.display.set_caption('Block Puzzle')
+GameBlockPuzzle.main_menu(win)
+# lost đè chữ block puzzle
